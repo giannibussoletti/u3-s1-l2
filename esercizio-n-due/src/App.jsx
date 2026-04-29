5181b4303505bf47cb4e6979ba6a0138b69c8de5
Gianni <BussolettiGianni@gmail.com>
2026-04-29 23:15:43 +0200

    Libreria 2: Electric Bogaloo

--- esercizio-n-due/src/App.jsx
+++ esercizio-n-due/src/App.jsx
@@ -1,5 +1,10 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
 import ListBook from "./component/ListBook"
+import MyNav from "./component/MyNav"
+import WindowAlert from "./component/WindowAlert"
+import MyFooter from "./component/MyFooter"
+
 import ScifiBooks from "./data/scifi.json"
 import RomanceBooks from "./data/romance.json"
 import HistoryBooks from "./data/history.json"
@@ -10,11 +15,21 @@ const App = function () {
   const AllTheBooks = ScifiBooks.concat(RomanceBooks, HistoryBooks, HorrorBooks, FantasyBooks)
   return (
     <>
-      <header></header>
-      <main>
+      <header>
+        <MyNav />
+      </header>
+      <main className="flex-grow-1 position-relative">
+        <div className="d-flex justify-content-center">
+          <WindowAlert />
+        </div>
+        <Container fluid className="p-0">
+          <img className="mb-5 w-100" src="../public/hero.jpg" />
+        </Container>
         <ListBook object={AllTheBooks} />
       </main>
-      <footer></footer>
+      <footer className="mt-4">
+        <MyFooter />
+      </footer>
     </>
   )
 }
