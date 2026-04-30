43e12db32f7c2392122c4b0680dc7170ecf34e38
Gianni <BussolettiGianni@gmail.com>
2026-04-30 12:55:42 +0200

    Update

--- esercizio/src/components/MyFooter.jsx
+++
@@ -1,9 +0,0 @@
-const MyFooter = function () {
-  return (
-    <div className="py-3 text-center" style={{ backgroundColor: "#234343" }}>
-      <p className="fw-bold fs-4 text-white m-0">Libreria</p>
-    </div>
-  )
-}
-
-export default MyFooter
