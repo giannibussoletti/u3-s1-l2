59f9fe81036119c516764addfb2dc454edf4782a
Gianni <BussolettiGianni@gmail.com>
2026-04-30 19:17:15 +0200

    little correction in import

--- esercizio-n-due/src/component/AddComment.jsx
+++ esercizio-n-due/src/component/AddComment.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react"
-import { Alert, Button, Form } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap"
 
 class AddComment extends Component {
   state = {
